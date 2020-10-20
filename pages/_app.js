8565bce37cd3d347f379c6bbfacfc9173f3e891a
phamhieu <phamhieu1998@gmail.com>
2020-10-20 08:39:01 +0800

    chore: access route paramon _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,15 @@
 import '../styles/globals.css'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+  const { slug } = router.query
+
+  useEffect(() => {
+    console.log('slug', slug)
+  }, [slug])
+
   return <Component {...pageProps} />
 }
 
